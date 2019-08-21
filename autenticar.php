23417537b0c20ce3c8fa2e299508bba2794149cb
Lucas Luana Carlos Edigio <avila.lucasflamenguista@gmail.com>
2019-08-21 15:54:39 -0300

    criei v2

---
+++ autenticar.php
@@ -0,0 +1,51 @@
+<?php
+if($_POST){
+session_start();
+$conn = mysqli_connect('localhost','root','');
+$banco = mysqli_select_db($conn,'sistemalogin');
+mysqli_set_charset($conn,'utf8');
+
+
+$email = $_POST['email'];
+$senha = md5($_POST['senha']);
+
+
+if (filter_var($email,FILTER_VALIDATE_EMAIL)) {
+$query = $conn->query ("SELECT * FROM cliente WHERE 
+      email = '{$email}' AND senha ='{$senha}'");
+   
+      
+ 
+      $row = $query->fetch_assoc();
+
+$count = $query->num_rows;
+if ($count>0){
+    
+    $_SESSION['userEmail'] = $row['email'];
+    $_SESSION['userId'] = $row ['id'];
+   header("location:home.php");
+}else{
+    
+    header("location:login.php?err=101");
+
+}
+
+}else{
+      
+        header("location:login.php?err=103");
+    
+    
+        
+        
+
+}
+
+
+
+}else{
+       
+        header("location:login.php?err=102");
+
+    }
+
+?>
